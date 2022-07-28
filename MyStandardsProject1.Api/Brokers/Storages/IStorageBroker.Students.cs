4499704a9a345a3b9689c7c6d889fda03c15a979
Khalid Usman <kusman@axsiumgroup.com>
2022-07-28 14:44:34 +0100

    BROKERS: Storage - Delete Student

--- MyStandardsProject1.Api/Brokers/Storages/IStorageBroker.Students.cs
+++ MyStandardsProject1.Api/Brokers/Storages/IStorageBroker.Students.cs
@@ -11,5 +11,6 @@ namespace MyStandardsProject1.Api.Brokers.Storages
         IQueryable<Student> SelectAllStudents();
         ValueTask<Student> SelectStudentByIdAsync(Guid studentId);
         ValueTask<Student> UpdateStudentAsync(Student student);
+        ValueTask<Student> DeleteStudentAsync(Student student);
     }
 }
