f740edb8a577a32713b4bc1a8182d3f444dac083
Khalid Usman <kusman@axsiumgroup.com>
2022-07-28 14:57:20 +0100

    ShouldThrowNotFoundExceptionOnRetrieveByIdIfStudentIsNotFoundAndLogItAsync -> PASS

--- MyStandardsProject1.Api/Services/Foundations/Students/StudentService.cs
+++ MyStandardsProject1.Api/Services/Foundations/Students/StudentService.cs
@@ -43,6 +43,8 @@ namespace MyStandardsProject1.Api.Services.Foundations.Students
                 Student maybeStudent = await this.storageBroker
                     .SelectStudentByIdAsync(studentId);
 
+                ValidateStorageStudent(maybeStudent, studentId);
+
                 return maybeStudent;
             });
     }
