55e96b893019bf4a21f05bf415525d59f9d84621
Khalid Usman <kusman@axsiumgroup.com>
2022-07-28 14:08:41 +0100

    BROKERS: Logging - Log LogError

--- MyStandardsProject1.Api/Brokers/Loggings/LoggingBroker.cs
+++ MyStandardsProject1.Api/Brokers/Loggings/LoggingBroker.cs
@@ -21,5 +21,8 @@ namespace MyStandardsProject1.Api.Brokers.Loggings
 
         public void LogWarning(string message) =>
             this.logger.LogWarning(message);
+
+        public void LogError(Exception exception) =>
+            this.logger.LogError(exception.Message, exception);
     }
 }
